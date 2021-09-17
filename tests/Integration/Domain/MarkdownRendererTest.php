3cb9bf3620990e41fed9a557349d973d0a35d765
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-09-18 00:34:36 +0200

    Extract UrlExpander

--- tests/Integration/Domain/MarkdownRendererTest.php
+++ tests/Integration/Domain/MarkdownRendererTest.php
@@ -48,42 +48,6 @@ class MarkdownRendererTest extends TestCase {
 			'https://professional.wiki/hi/example.md',
 			'https://professional.wiki/hi/kittens.md'
 		];
-
-		yield 'Relative URL with path' => [
-			'amazing/fluff/kittens.md',
-			'https://professional.wiki/hi/example.md',
-			'https://professional.wiki/hi/amazing/fluff/kittens.md',
-		];
-
-		yield 'Relative URL with preceding slash' => [
-			'/kittens.md',
-			'https://professional.wiki/hi/example.md',
-			'https://professional.wiki/hi/kittens.md'
-		];
-
-		yield 'Relative URL followed by slash' => [
-			'kittens.md/',
-			'https://professional.wiki/hi/example.md',
-			'https://professional.wiki/hi/kittens.md'
-		];
-
-		yield 'Empty relative URL' => [
-			'',
-			'https://professional.wiki/hi/example.md',
-			'https://professional.wiki/hi/'
-		];
-
-		yield 'Relative URL with image' => [
-			'kittens.gif',
-			'https://professional.wiki/hi/example.md',
-			'https://professional.wiki/hi/kittens.gif'
-		];
-
-		yield 'Relative URL with no slash content URL' => [
-			'kittens.md',
-			'https://professional.wiki',
-			'https://professional.wiki/kittens.md'
-		];
 	}
 
 }
