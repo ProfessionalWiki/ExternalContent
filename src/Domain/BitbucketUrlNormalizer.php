bdc42df190f74b46b8e3cc62022aee5919e34a04
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-09-18 18:17:44 +0200

    Finish baseline BitbucketUrlNormalizer implementation

--- src/Domain/BitbucketUrlNormalizer.php
+++ src/Domain/BitbucketUrlNormalizer.php
@@ -9,25 +9,43 @@ class BitbucketUrlNormalizer implements UrlNormalizer {
 	// TODO: inject default branch
 
 	public function normalize( string $url ): string {
+		$parsedUrl = parse_url( $url );
+
+		/** @psalm-suppress PossiblyUndefinedArrayOffset */
+		$parsedUrl['path'] = $this->normalizePath( $parsedUrl['path'] );
+
+		return $this->buildUrl( $parsedUrl );
+	}
+
+	private function normalizePath( string $url ): string {
+		// /projects/KNOW/repos/kittens/browse/Arbitrary.md
 		$urlParts = explode( '/', $url );
-		$urlParts[] = $this->defaultLastPart( array_pop( $urlParts ) );
+
+		if ( in_array( $urlParts[5] ?? '', [ 'browse', '' ] ) ) {
+			$urlParts[5] = 'raw';
+		}
+
+		$urlParts[6] = ( $urlParts[6] ?? '' ) === '' ? 'README.md' : $urlParts[6];
+
 		return implode( '/', $urlParts );
 	}
 
-	private function defaultLastPart( string $urlPart ): string {
-		if ( $urlPart === '' ) {
-			return 'README.md';
-		}
-
-		if ( $this->isDirectory( $urlPart ) ) {
-			 return $urlPart . '/README.md';
-		}
-
-		return $urlPart;
-	}
-
-	private function isDirectory( string $urlPart ): bool {
-		return !str_contains( $urlPart, '.' );
+	/**
+	 * Modernized version of https://www.php.net/manual/en/function.parse-url.php#106731
+	 * @psalm-suppress MixedOperand
+	 * @psalm-suppress MixedAssignment
+	 */
+	private function buildUrl( array $parsedUrl ): string {
+		$scheme = isset( $parsedUrl['scheme'] ) ? $parsedUrl['scheme'] . '://' : '';
+		$host = $parsedUrl['host'] ?? '';
+		$port = isset( $parsedUrl['port'] ) ? ':' . $parsedUrl['port'] : '';
+		$user = $parsedUrl['user'] ?? '';
+		$pass = isset( $parsedUrl['pass'] ) ? ':' . $parsedUrl['pass'] : '';
+		$pass = ( $user || $pass ) ? "$pass@" : '';
+		$path = $parsedUrl['path'] ?? '';
+		$query = isset( $parsedUrl['query'] ) ? '?' . $parsedUrl['query'] : '';
+		$fragment = isset( $parsedUrl['fragment'] ) ? '#' . $parsedUrl['fragment'] : '';
+		return "$scheme$user$pass$host$port$path$query$fragment";
 	}
 
 }
--- tests/Unit/Domain/BitbucketUrlNormalizerTest.php
+++ tests/Unit/Domain/BitbucketUrlNormalizerTest.php
@@ -23,29 +23,55 @@ class BitbucketUrlNormalizerTest extends TestCase {
 	}
 
 	public function normalizationProvider(): iterable {
-		yield 'Directory should get README.md appended' => [
-			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/browse',
-			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/browse/README.md'
+		yield 'Raw paths should not be changed' => [
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/raw/Arbitrary.md',
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/raw/Arbitrary.md'
 		];
 
-		// TODO: what about path/LICENSE?
+		yield 'Browse paths get turned into raw paths' => [
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/browse/Arbitrary.md',
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/raw/Arbitrary.md'
+		];
 
-		yield 'Directory with slash should get README.md appended' => [
+		yield 'Repository root defaults to README.md' => [
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/browse',
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/raw/README.md'
+		];
+
+		yield 'Repository root defaults to README.md (tailing slash)' => [
 			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/browse/',
-			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/browse/README.md'
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/raw/README.md'
 		];
 
 		yield 'README.md file should remain unchanged' => [
 			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/browse/README.md',
-			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/browse/README.md'
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/raw/README.md'
 		];
 
-		yield 'Arbitrary.md file should remain unchanged' => [
-			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/browse/Arbitrary.md',
-			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/browse/Arbitrary.md'
+		yield 'Files without dot are handled' => [
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/browse/LICENSE',
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/raw/LICENSE'
 		];
 
-		// TODO: https://git.example.com/projects/KNOW/repos/fluffy-kittens/raw/README.md?at=refs%2Fheads%2Fmain
+		yield 'Branch query should remain unchanged' => [
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/browse?at=refs%2Fheads%2FMyBranch',
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/raw/README.md?at=refs%2Fheads%2FMyBranch'
+		];
+
+		yield 'Browse is appended when missing' => [
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens',
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/raw/README.md'
+		];
+
+		yield 'Browse is appended when missing (tailing slash)' => [
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/',
+			'https://git.example.com/projects/KNOW/repos/fluffy-kittens/raw/README.md'
+		];
+
+		yield 'Port is retained' => [
+			'http://localhost:7990/projects/MYP/repos/fluffykittens/browse',
+			'http://localhost:7990/projects/MYP/repos/fluffykittens/raw/README.md'
+		];
 	}
 
 }
