fd9caa3d2fc48e68857b2d84b52f5ccdc8a95eab
Muhammad Yousuf Fazal <m.yousuf.fazal@gmail.com>
2023-10-18 03:58:18 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <m.yousuf.fazal@gmail.com>

--- tests/Integration/Domain/CodeRendererTest.php
+++ tests/Integration/Domain/CodeRendererTest.php
@@ -15,35 +15,35 @@ class CodeRendererTest extends TestCase {
 	public function testRendersSimplePhpWithoutLineNumbers(): void {
 		$this->assertSame(
 			'<pre class="external-content" data-toolbar-order="copy-to-clipboard"><code class="language-php">print( "Hello world" );</code></pre>',
-			( new CodeRenderer( language: 'php', showLineNumbers: false ) )->render( 'print( "Hello world" );', '' )
+			( new CodeRenderer( language: 'php', showLineNumbers: false, showSpecificLines: '' ) )->render( 'print( "Hello world" );', '' )
 		);
 	}
 
 	public function testRendersSimpleTypescriptWithLineNumbers(): void {
 		$this->assertSame(
 			'<pre class="external-content line-numbers" data-toolbar-order="copy-to-clipboard"><code class="language-typescript">console.log( "Hello world" as string );</code></pre>',
-			( new CodeRenderer( language: 'typescript', showLineNumbers: true ) )->render( 'console.log( "Hello world" as string );', '' )
+			( new CodeRenderer( language: 'typescript', showLineNumbers: true, showSpecificLines: '' ) )->render( 'console.log( "Hello world" as string );', '' )
 		);
 	}
 
 	public function testRendersEscapedHtml(): void {
 		$this->assertSame(
 			'<pre class="external-content" data-toolbar-order="copy-to-clipboard"><code class="language-html">&lt;script>alert( "HAX" );&lt;/script></code></pre>',
-			( new CodeRenderer( language: 'html', showLineNumbers: false ) )->render( '<script>alert( "HAX" );</script>', '' )
+			( new CodeRenderer( language: 'html', showLineNumbers: false, showSpecificLines: '' ) )->render( '<script>alert( "HAX" );</script>', '' )
 		);
 	}
 
 	public function testRendersEscapedHtmlWithWrongLanguage(): void {
 		$this->assertSame(
 			'<pre class="external-content" data-toolbar-order="copy-to-clipboard"><code class="language-javascript">&lt;script>alert( "HAX" );&lt;/script></code></pre>',
-			( new CodeRenderer( language: 'javascript', showLineNumbers: false ) )->render( '<script>alert( "HAX" );</script>', '' )
+			( new CodeRenderer( language: 'javascript', showLineNumbers: false, showSpecificLines: '' ) )->render( '<script>alert( "HAX" );</script>', '' )
 		);
 	}
 
 	public function testRendersEscapedPartialHtml(): void {
 		$this->assertSame(
 			'<pre class="external-content" data-toolbar-order="copy-to-clipboard"><code class="language-html">&lt;/code>&lt;/pre>FOO</code></pre>',
-			( new CodeRenderer( language: 'html', showLineNumbers: false ) )->render( '</code></pre>FOO', '' )
+			( new CodeRenderer( language: 'html', showLineNumbers: false, showSpecificLines: '' ) )->render( '</code></pre>FOO', '' )
 		);
 	}
 
