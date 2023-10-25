8b1a1dcac2fb8150c3d631e143c48092b88e0ecb
Muhammad Yousuf Fazal <m.yousuf.fazal@gmail.com>
2023-10-25 12:47:48 +0500

    Fixed for other Language Code Files
    Signed-off-by: Muhammad Yousuf Fazal <m.yousuf.fazal@gmail.com>

--- resources/code-specific-lines.js
+++ resources/code-specific-lines.js
@@ -1,17 +1,34 @@
-Prism.hooks.add( 'complete', function ( env ) {
+Prism.hooks.add( 'complete', function( env ) {
     var showLineRanges = env.element.parentElement.getAttribute( 'data-show-lines' );
 
     if (showLineRanges) {
-        var showLines = [];
+        var showLines = showLines( showLineRanges );
 
-        showLineRanges.replace( /\s+/g, '' ).split( ',' ).filter( Boolean ).forEach( function ( currentRange ) {
+        var tokenList = tokenList( env.element.innerHTML, showLines );
+
+        var lineNumbersActive = Prism.util.isActive( env.element, 'line-numbers' );
+        
+        if ( lineNumbersActive ) {
+            var lineNumberList = lineNumberList( env.element.querySelectorAll( '.line-numbers-rows span' ), showLines );
+
+            env.element.innerHTML = tokenList.join( '' ) + '<span aria-hidden="true" class="line-numbers-rows">' + lineNumberList.join( '' ) + '</span>';
+        }
+        else {
+            env.element.innerHTML = tokenList.join( '' );
+        }
+    }
+
+    function showLines( showLineRanges ) {
+        var showLines = [];
+    
+        showLineRanges.replace( /\s+/g, '' ).split( ',' ).filter( Boolean ).forEach( function( currentRange ) {
             if (currentRange.includes( '-' )) {
                 var range = currentRange.split( '-' );
-
+    
                 if ( range[0] > range[1] ) {
                     [range[0], range[1]] = [range[1], range[0]];
                 }
-
+    
                 for ( var counter = range[0]; counter <= range[1]; counter++ ) {
                     showLines.push( Number( counter ) );
                 }
@@ -20,28 +37,42 @@ Prism.hooks.add( 'complete', function ( env ) {
                 showLines.push( Number( currentRange ) );
             }
         });
-
-        var tokenList = env.element.querySelector( '.token' ).innerHTML.split( /\n(?!$)/g );
-
-        var lineNumbersActive = Prism.util.isActive(env.element, 'line-numbers');
-        
-        if ( lineNumbersActive ) {
-            var lineNumberRows = env.element.querySelectorAll( '.line-numbers-rows span' );
-        }
-
-        tokenList.forEach ( function( value, index ) {
+    
+        return showLines;
+    }
+    
+    function tokenList( HTML, showLines ) {
+        var tokenList = HTML.split( /\n(?!$)/g );
+        var tokenCount = tokenList.length;
+        const regex = /(<span aria-hidden="true" class="line-numbers-rows">(.*<\/span>))/gm;
+    
+        tokenList.forEach( function( value, index ) {
+            if ( index === tokenCount - 1 ) {
+                value = value.replace (regex, '' );
+            }
+    
             if ( showLines.includes ( index + 1 ) ) {
                 tokenList[index] += '\n';
             }
             else {
                 tokenList[index] = '<span class="hide-line">' + value + '</span>';
-
-                if ( lineNumbersActive ) {
-                    lineNumberRows[index].classList.add( 'hide-line' );
-                }
             }
         });
-
-        env.element.querySelector( '.token' ).innerHTML = tokenList.join( '' );
+    
+        return tokenList;
+    }
+    
+    function lineNumberList( lineNumberRows, showLines ) {
+        var lineNumberList = [];
+    
+        lineNumberRows.forEach( function( value, index ) {
+            if ( !showLines.includes( index + 1 ) ) {
+                lineNumberRows[index].classList.add( 'hide-line' );
+            }
+    
+            lineNumberList[index] = lineNumberRows[index].outerHTML;
+        });
+    
+        return lineNumberList;
     }
 });
