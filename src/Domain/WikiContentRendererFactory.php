64491c186c68162fddd8b2b0a606f3de20267ba8
Muhammad Yousuf Fazal <m.yousuf.fazal@gmail.com>
2023-10-30 18:46:20 +0500

    If `lang` defined, render as code, otherwise check `render` and file is `md`, then render markdown else render code
    Signed-off-by: Muhammad Yousuf Fazal <m.yousuf.fazal@gmail.com>

--- src/Domain/WikiContentRendererFactory.php
+++ src/Domain/WikiContentRendererFactory.php
@@ -11,12 +11,21 @@ use ProfessionalWiki\ExternalContent\Domain\ContentRenderer\RendererConfig;
 class WikiContentRendererFactory implements ContentRendererFactory {
 
 	public function createContentRenderer( RendererConfig $config ): ContentRenderer {
-		if ( $config->render ) {
+		if ( $config->language !== '' ) {
+			return new CodeRenderer(
+				language: $config->language,
+				showLineNumbers: $config->showLineNumbers,
+				showSpecificLines: $config->showSpecificLines,
+				showEditButton: $config->showEditButton
+			);
+		}
+
+		if ( $config->render && $config->fileExtension === 'md' ) {
 			return new MarkdownRenderer();
 		}
 
 		return new CodeRenderer(
-			language: ( $config->language !== '' ) ? $config->language : $config->fileExtension, // TODO: Use an extension-to-language map, although common extensions already work.
+			language: $config->fileExtension, // TODO: Use an extension-to-language map, although common extensions already work.
 			showLineNumbers: $config->showLineNumbers,
 			showSpecificLines: $config->showSpecificLines,
 			showEditButton: $config->showEditButton
